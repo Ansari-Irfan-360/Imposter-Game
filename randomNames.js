6a494f3dc9db048ec8ad0a79288d9cacf5e6939c
Ansari Irfan <115148633+Ansari-Irfan-360@users.noreply.github.com>
2024-12-03 19:44:32 +0530

    Create randomNames.js

---
+++ randomNames.js
@@ -0,0 +1,15 @@
+const randomNames = [
+  "James", "John", "Robert", "Michael", "William", "David", "Joseph", "Charles", "Thomas", "Daniel",
+  "Matthew", "George", "Andrew", "Edward", "Henry", "Samuel", "Jack", "Oliver", "Benjamin", "Lucas",
+  "Alexander", "Ryan", "Joseph", "Liam", "Nathan", "Ethan", "Caleb", "Christopher", "Aiden", "Isaac",
+  "Aaron", "Oscar", "Leon", "Adam", "Max", "Felix", "Miles", "Victor", "George", "Jacob",
+  "Arjun", "Aarav", "Vikram", "Rohan", "Ishaan", "Ayaan", "Siddharth", "Karan", "Manish", "Rahul",
+  "Dev", "Amit", "Raj", "Vishal", "Sanjay", "Anil", "Vikrant", "Aditya", "Harsh", "Shiv",
+  "Neel", "Kabir", "Parth", "Omkar", "Krish", "Ujjwal", "Rishi", "Avi", "Abhishek", "Suresh",
+  "Pranav", "Shivansh", "Tushar", "Aakash", "Kunal", "Ajay", "Deepak", "Nikhil", "Yash", "Manav",
+  "Tanmay", "Rajesh", "Kartik", "Ankur", "Siddhi", "Keshav", "Rajat", "Pritam", "Tarun", "Sandeep",
+  "Harish", "Sachin", "Jay", "Madhav", "Raghav", "Arihant", "Vivek", "Suraj", "Ashwin", "Raghul",
+  "Hassan", "Shaurya", "Lakshya", "Gautam", "Kiran", "Niranjan", "Rupesh", "Tejas", "Yogesh", "Ishwar",
+  "Suryansh", "Aman", "Keshav", "Karanveer", "Madhur", "Sahil", "Sumeet", "Ansh", "Prathamesh", "Veer",
+  "Bharat", "Tariq", "Krishna", "Harvinder", "Kunal", "Alok", "Manoj", "Siddharth", "Reyansh", "Rohit"
+];
